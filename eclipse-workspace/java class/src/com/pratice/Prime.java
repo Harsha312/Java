1eaa405b61831e69b5ab3bb5c72b193aa262132d
Harsha312 <sriharshadevabattina@gmail.com>
2018-05-01 16:23:42 -0500

    Prime

---
+++ eclipse-workspace/java class/src/com/pratice/Prime.java
@@ -0,0 +1,14 @@
+package com.pratice;
+
+public class Prime {
+
+	public Prime() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
