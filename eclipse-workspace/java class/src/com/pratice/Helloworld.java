41bef2bf3dadead75a4dc0c4a91f448f53e22934
Harsha312 <sriharshadevabattina@gmail.com>
2018-05-08 15:47:10 -0500

    int, double, string

--- eclipse-workspace/java class/src/com/pratice/Helloworld.java
+++ eclipse-workspace/java class/src/com/pratice/Helloworld.java
@@ -14,8 +14,17 @@ public class Helloworld {
 		System.out.println("Enter some number");
 		int harsha = Scan.nextInt();
        System.out.println("THe entered value is "+harsha);
+       
+       Scanner Scan1 = new Scanner(System.in);
+		System.out.println("Enter some String");
+		String harsha2 = Scan1.nextLine();
+      System.out.println("THe entered String is "+harsha2);  
      
-   
+        Scanner Scan2 = new Scanner (System.in);
+        System.out.println("Enter some decimal");
+		double harsha1 = Scan2.nextDouble();
+       System.out.println("THe entered value is "+harsha1);
+        
 	}
 	
 	
